91ed6c769c05e678d68154d29eeedf07d06bd25c
jimytc <solofat@gmail.com>
2020-12-03 22:22:35 +0800

    Project Init

---
+++ lib/aoc2020_elixir.ex
@@ -0,0 +1,18 @@
+defmodule Aoc2020Elixir do
+  @moduledoc """
+  Documentation for `Aoc2020Elixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Aoc2020Elixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/aoc2020_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule Aoc2020ElixirTest do
+  use ExUnit.Case
+  doctest Aoc2020Elixir
+
+  test "greets the world" do
+    assert Aoc2020Elixir.hello() == :world
+  end
+end
